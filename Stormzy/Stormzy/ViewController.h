fdd3f6f4eebb2a32d7d1f7158646d57d7871610c
Mesfin Bekele Mekonnen <mesbekmek@gmail.com>
2016-04-12 13:08:14 -0400

    Added project file

---
+++ Stormzy/Stormzy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Stormzy
+//
+//  Created by Mesfin Bekele Mekonnen on 4/12/16.
+//  Copyright © 2016 Mesfin Bekele Mekonnen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Stormzy/StormzyTests/StormzyTests.m
@@ -0,0 +1,39 @@
+//
+//  StormzyTests.m
+//  StormzyTests
+//
+//  Created by Mesfin Bekele Mekonnen on 4/12/16.
+//  Copyright © 2016 Mesfin Bekele Mekonnen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface StormzyTests : XCTestCase
+
+@end
+
+@implementation StormzyTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
